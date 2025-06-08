52f73154af03b4baed34aff008a9a66e1827c4bb
avinashkmr301 <avinashkmr301@gmail.com>
2025-06-08 22:46:23 +0530

    unit test using mockito test

--- mockito-demo/src/test/java/com/avi/mockito/mockito_demo/business/SomeBusinessImplMockTest.java
+++ mockito-demo/src/test/java/com/avi/mockito/mockito_demo/business/SomeBusinessImplMockTest.java
@@ -14,8 +14,21 @@ class SomeBusinessImplMockTest {
         DataService dataServiceMock = mock(DataService.class);
         //dataServiceMock.retrieveAllData() => new int[]{24,44,55};
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{24,44,55});
+
         SomeBusinessImpl business = new SomeBusinessImpl(dataServiceMock);
         int greatestValueFromAllData = business.findGreatestFromAllData();
         assertEquals(55, greatestValueFromAllData);
+    }
+
+    @Test
+    void testFindGreatestFromAllData_WithOneValue(){
+
+        //mocking
+        DataService dataServiceMock = mock(DataService.class);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{22});
+        //simple creating obj, calling method
+        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataServiceMock);
+        int greatestFromAllData = someBusiness.findGreatestFromAllData();
+        assertEquals(22,greatestFromAllData);
     }
 }
