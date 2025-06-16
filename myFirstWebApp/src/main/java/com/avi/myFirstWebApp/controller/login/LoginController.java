34352b499a2f49d1ee6459ff8e371ebe6fe2f9bb
avinashkmr301 <avinashkmr301@gmail.com>
2025-06-16 22:48:02 +0530

    logger level

--- myFirstWebApp/src/main/java/com/avi/myFirstWebApp/controller/login/LoginController.java
+++ myFirstWebApp/src/main/java/com/avi/myFirstWebApp/controller/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.avi.myFirstWebApp.controller.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // http://localhost:8080/login?name=Avi
     @RequestMapping("/login")
     public String login(@RequestParam String name, ModelMap modelMap){
         modelMap.put("name", name);
+
+        logger.debug("Request Param is {}", name);
+        logger.info("Request Param is at info level");
+        logger.warn("Request Param is at waring level");
+
         return "login";
     }
 }
