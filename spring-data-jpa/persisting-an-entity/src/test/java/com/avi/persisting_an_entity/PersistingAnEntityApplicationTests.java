d9e43ad568fc5ffa507ec6c1029fa0294c8c5deb
avinashkmr301 <avinashkmr301@gmail.com>
2025-06-22 11:42:56 +0530

    cmmit

---
+++ spring-data-jpa/persisting-an-entity/src/test/java/com/avi/persisting_an_entity/PersistingAnEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avi.persisting_an_entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistingAnEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
