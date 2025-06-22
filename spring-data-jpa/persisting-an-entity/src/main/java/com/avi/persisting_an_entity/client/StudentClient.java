bbb654aa7d53f11940cf1764b9419e4600363243
avinashkmr301 <avinashkmr301@gmail.com>
2025-06-22 19:06:21 +0530

    findById and delete

--- spring-data-jpa/persisting-an-entity/src/main/java/com/avi/persisting_an_entity/client/StudentClient.java
+++ spring-data-jpa/persisting-an-entity/src/main/java/com/avi/persisting_an_entity/client/StudentClient.java
@@ -7,6 +7,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class StudentClient implements ApplicationRunner {
 
@@ -22,5 +24,13 @@ public class StudentClient implements ApplicationRunner {
         //Updating a student
         returnedStudent.setName("Thanuja P");
         Student updatedStudent = studentRepository.save(returnedStudent);
+
+        //findById
+        Optional<Student> studentOptional = studentRepository.findById(1L);
+        System.out.println(studentOptional.get());
+
+        //delete
+        System.out.println("deleting...");
+        studentRepository.delete(studentOptional.get());
     }
 }
