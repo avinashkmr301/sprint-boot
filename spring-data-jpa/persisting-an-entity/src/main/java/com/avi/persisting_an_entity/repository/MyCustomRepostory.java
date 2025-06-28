7188c4256bb47e9e904f91baa33d0bd220d6fd61
avinashkmr301 <avinashkmr301@gmail.com>
2025-06-28 14:59:27 +0530

    removed unused class

--- spring-data-jpa/persisting-an-entity/src/main/java/com/avi/persisting_an_entity/repository/MyCustomRepostory.java
+++ spring-data-jpa/persisting-an-entity/src/main/java/com/avi/persisting_an_entity/repository/MyCustomRepostory.java
@@ -1,27 +1,16 @@
 package com.avi.persisting_an_entity.repository;
 
+import com.avi.persisting_an_entity.entity.Student;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.Repository;
-
-import com.avi.persisting_an_entity.entity.Student;
-
-public interface MyCustomRepostory extends Repository<Student, Long> {
-
-    // Custom method to save a student
+public interface MyCustomRepostory extends Repository {
     Student save(Student student);
 
-    // Custom method to find a student by their ID
-    Optional<Student> findById(Long id);
+    Optional<Object> findById(long l);
 
-    // Custom method to find all students
     List<Student> findAll();
 
-    // Custom method to find a student by their enrollment ID
-    Student findByEnrollmentId(String enrollmentId);
-
-    // Custom method to delete a student by their enrollment ID
-    void deleteByEnrollmentId(String enrollmentId);
-    
 }
