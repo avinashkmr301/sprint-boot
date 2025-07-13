d9292b60a4b54dbb8511ff895cd2272b4671ac0d
avinashkmr301 <avinashkmr301@gmail.com>
2025-07-13 10:37:50 +0530

    basic-derived-query

---
+++ spring-data-jpa/basic-derived-queries/src/main/java/com/avi/basic_jpa_query/BasicDerivedQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.avi.basic_jpa_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDerivedQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicDerivedQueriesApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa/basic-derived-queries/src/test/java/com/avi/basic_jpa_query/BasicDerivedQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avi.basic_jpa_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicDerivedQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
